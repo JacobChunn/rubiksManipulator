66f0c1405ba77821e39d81529a69d209e7092802
Jacob Chunn <chunnjake@gmail.com>
2023-01-21 02:46:22 -0500

    Fix cube struct layout

--- cube3x3x7.cpp
+++ cube3x3x7.cpp
@@ -3,16 +3,19 @@ enum InitType { Default, Empty };
 enum Direction { Back, Front, Left, Right, Up, Down};
 
 struct Cubie {
-	Direction direction;
 	Color* colors;
 };
 
 struct Corner {
+	Direction dirPrimary;
+	Direction dirSecondary;
 	Cubie* cubies;
 };
 
 struct Edge {
 	bool isLong;
+	Direction dirPrimary;
+	Direction dirSecondary;
 	Cubie* cubies;
 };
 
@@ -120,74 +123,99 @@ class Cube {
 			}
 		}
 
-		void fillCorner(Corner* c, Direction dir, Color colA, Color colB,
-			 Color colC) {
+		void fillCorner(Corner* c, Color colA, Color colB, Color colC,
+			Direction colAdir, Direction colCdir) {
 
+			c->cubies[0].direction = colCdir;
+			c->cubies[0].colors[0] = colA;
+			c->cubies[0].colors[1] = colB;
 			c->cubies[0].colors[2] = colC;
-			for (int i = 0; i < 3; i++) {
-				c->cubies[i].direction = dir;
+			for (int i = 1; i < 3; i++) {
+				c->cubies[i].direction = colAdir;
 				c->cubies[i].colors[0] = colA;
 				c->cubies[i].colors[1] = colB;
 			}
 		}
 
-		void fillLongEdge(Edge* e, Direction dir, Color colA, Color colB) {
+		void fillLongEdge(Edge* e, Color colA, Color colB, Direction colAdir,
+			Direction colBdir) {
 
+			e->cubies[0].direction = colBdir;
+			e->cubies[0].colors[0] = colA;
 			e->cubies[0].colors[1] = colB;
-			for (int i = 0; i < 3; i++) {
-				e->cubies[i].direction = dir;
+			for (int i = 1; i < 3; i++) {
+				e->cubies[i].direction = colAdir;
 				e->cubies[i].colors[0] = colA;
 			}
 		}
 
-		void fillShortEdge(Edge* e, Direction dir, Color colA, Color colB) {
+		void fillShortEdge(Edge* e, Color colA, Color colB, Direction colAdir) {
 
-			e->cubies[0].direction = dir;
+			e->cubies[0].direction = colAdir;
 			e->cubies[0].colors[0] = colA;
 			e->cubies[0].colors[1] = colB;
 		}
 
+		template <typename T>
+		void cycle(T* arr, int arrLen) {
+
+		}
+
 		void front()
-		void fronti()
-		void front2()
-		void front2i()
-		void front3()
-		void front3i()
-
 		void back()
-		void backi()
-		void back2()
-		void back2i()
-		void back3()
-		void back3i()
-
 		void left()
-		void lefti()
-		void left2()
-		void left2i()
-		void left3()
-		void left3i()
-
 		void right()
-		void righti()
-		void right2()
-		void right2i()
-		void right3()
-		void right3i()
-
 		void up()
-		void upi()
-		void up2()
-		void up2i()
-		void up3()
-		void up3i()
-
 		void down()
+
+		void fronti()
+		void backi()
+		void lefti()
+		void righti()
+		void upi()
 		void downi()
-		void down2()
-		void down2i()
-		void down3()
-		void down3i()
+
+		void frontS1()
+		void backS1()
+		void leftS1()
+		void rightS1()
+		void upS1()
+		void downS1()
+
+		void frontS1i()
+		void backS1i()
+		void leftS1i()
+		void rightS1i()
+		void upS1i()
+		void downS1i()
+
+		void frontS2()
+		void backS2()
+		void leftS2()
+		void rightS2()
+		void upS2()
+		void downS2()
+
+		void frontS2i()
+		void backS2i()
+		void leftS2i()
+		void rightS2i()
+		void upS2i()
+		void downS2i()
+
+		void frontS3()
+		void backS3()
+		void leftS3()
+		void rightS3()
+		void upS3()
+		void downS3()
+
+		void frontS3i()
+		void backS3i()
+		void leftS3i()
+		void rightS3i()
+		void upS3i()
+		void downS3i()
 
 		~Cube() {
 
